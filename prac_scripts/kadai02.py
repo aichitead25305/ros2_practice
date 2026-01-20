07fca4904190ad9a81bd99620829184f7300d09b
aichitead25305 <aichitead25305@gmail.com>
2026-01-20 10:02:57 +0900

    init commit

---
+++ prac_scripts/kadai02.py
@@ -0,0 +1,5 @@
+x = int(input())
+if (x >= 5) :
+	print("x is 5 or greater")
+else:
+	print("x is less than 5")
---
+++ prac_scripts/kadai04.py
@@ -0,0 +1,5 @@
+def greet(name):
+	print("Hello " + name )
+
+greet(name = "AAA")
+greet(name = "BBB")
